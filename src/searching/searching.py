edd9541bda06aff2c7aaf8964944a3afe1aae15b
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-08-18 12:15:38 -0500

    almost done

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,5 +1,11 @@
 def linear_search(arr, target):
     # Your code here
+    for i in range(len(arr)):
+
+        if arr[i] == target:
+            return i
+        
+
 
 
     return -1   # not found
@@ -9,6 +15,16 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    hi = len(arr)
+    lo = 0
+
+    mid = (hi + lo) // 2
+
+    if arr[mid] < target:
+
+    elif arr[mid] > target:
+        
+
 
 
     return -1  # not found
